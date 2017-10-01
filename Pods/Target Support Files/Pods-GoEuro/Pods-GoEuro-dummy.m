e65fd437398b55503472f4f3ad4fe27584df7295
farrag <farrag91@icloud.com>
2017-10-01 09:06:51 +0200

    f

--- Pods/Target Support Files/Pods-GoEuro/Pods-GoEuro-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoEuro : NSObject
-@end
-@implementation PodsDummy_Pods_GoEuro
-@end
--- Pods/Target Support Files/Pods-testApp/Pods-testApp-umbrella.h
+++ Pods/Target Support Files/Pods-testApp/Pods-testApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_GoEuroVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_GoEuroVersionString[];
+FOUNDATION_EXPORT double Pods_testAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_testAppVersionString[];
 
